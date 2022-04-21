0d3090da8b39395ce8e1e9f5f33e06f8c6090fe1
devadrian16 <adri__16@hotmail.com>
2022-04-21 15:43:16 +0200

    migration favoritos

---
+++ database/migrations/2014_10_12_000000_create_favoritos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favoritos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_league');
+            $table->integer('id_team');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favoritos');
+    }
+};
