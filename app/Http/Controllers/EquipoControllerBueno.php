088989bb7cf4c6df6f79cf06210ce3cf612869c2
devadrian16 <adri__16@hotmail.com>
2022-05-04 14:30:19 +0200

    fetch equipos

---
+++ app/Http/Controllers/EquipoControllerBueno.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use GuzzleHttp\Client;
+use App\Models\Favorito;
+use Auth;
+
+class EquipoController extends Controller
+{
+    private $api;
+
+    public function __construct(Client $client)
+    {
+        $this->api = new ApiController($client);
+    }
+
+    public function verEquipo($id)
+    {
+        //Equipo
+        $team = $this->api->getTeam($id);
+        $id_league = $this->api->getLeagueTeam($id);
+
+        //Jugadores
+        $porteros = [];
+        $defensas = [];
+        $centrocampistas = [];
+        $delanteros = [];
+
+        $players = $this->api->getPlayersTeam($id, $id_league['league']['id']);
+
+        for ($i = 1; $i <= $players['paging']['total']; $i++) {
+            $page = $this->api->getPlayersTeamPage($id, $id_league['league']['id'], $i);
+
+            foreach ($page['response'] as $player) {
+
+                switch ($player['statistics'][0]['games']['position']) {
+                    case 'Attacker':
+                        array_push($delanteros, $player);
+                        break;
+                    case 'Midfielder':
+                        array_push($centrocampistas, $player);
+                        break;
+                    case 'Defender':
+                        array_push($defensas, $player);
+                        break;
+                    case 'Goalkeeper':
+                        array_push($porteros, $player);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        if (Auth::check()) {
+            //Favoritos
+            $favorito = Favorito::where('id_user', Auth::user()->id)->where('id_team', $id);
+
+            return view('equipo', ['team' => $team['team'], 'favorito' => $favorito, 'delanteros' => $delanteros, 'centrocampistas' => $centrocampistas, 'defensas' => $defensas, 'porteros' => $porteros]);
+        } else {
+            return view('equipo', ['team' => $team['team'], 'delanteros' => $delanteros, 'centrocampistas' => $centrocampistas, 'defensas' => $defensas, 'porteros' => $porteros]);
+        }
+    }
+
+    public function guardarEquipoFav($id)
+    {
+        $favorito = new Favorito();
+
+        $favorito->id_user = Auth::user()->id;
+
+        $team = $this->api->getTeam($id);
+        $favorito->id_team = $team['team']['id'];
+        $favorito->name_team = $team['team']['name'];
+
+        $id_league = $this->api->getLeagueTeam($id);
+        $favorito->id_league = $id_league['league']['id'];
+        $favorito->name_league = $id_league['league']['name'];
+
+        $favorito->save();
+
+        return redirect()->route('verEquipo', ['team' => $id]);
+    }
+
+    public function eliminarEquipoFav($id)
+    {
+        $favorito = Favorito::where('id_user', Auth::user()->id)->where('id_team', $id);
+        $favorito->delete();
+
+        return redirect()->route('verEquipo', ['team' => $id]);
+    }
+}
---
+++ resources/views/equipoBueno.blade.php
@@ -0,0 +1,214 @@
+@extends('layouts.api')
+
+@section('title', $team['name'])
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+@endsection
+
+@section('content')
+
+<div class="row justify-content-around">
+    <div class="col-lg-10 my-4">
+        <div class="card">
+            <div class="card-header fs-2">
+                <a style="text-decoration: none; color: black;">
+                    <img src="{{ $team['logo'] }}" alt="" style="width: 75px; height: 75px;">
+                </a>
+                {{ $team['name'] }}
+                @auth
+                @if($favorito->count() == 0)
+                <div id="nofavorito" style="font-size: 1em; display: inline-block;">
+                    <a href="/save/{{ $team['id'] }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#c8cdcd" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div id="favorito" class="d-none" style="font-size: 1em; display: inline-block;">
+                    <a href="/delete/{{ $team['id'] }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffcd00" class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                    </a>
+                </div>
+                @else
+                <div id="nofavorito" class="d-none" style="font-size: 1em; display: inline-block;">
+                    <a href="/save/{{ $team['id'] }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#c8cdcd" class="bi bi-star" viewBox="0 0 16 16">
+                            <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                        </svg>
+                    </a>
+                </div>
+
+                <div id="favorito" style="font-size: 1em; display: inline-block;">
+                    <a href="/delete/{{ $team['id'] }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffcd00" class="bi bi-star-fill" viewBox="0 0 16 16">
+                            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                        </svg>
+                    </a>
+                </div>
+                @endif
+                @endauth
+            </div>
+
+            <div class="card-body">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-10 my-4">
+        <div class="card">
+            <div class="card-header">Plantilla del {{ $team['name'] }} en su liga</div>
+
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <th colspan="9" class="fs-3">Porteros</th>
+                            </tr>
+                            <tr class="text-center" style="background-color: #eeeeee;">
+                                <td></td>
+                                <td>Nombre</td>
+                                <td>Edad</td>
+                                <td>Altura</td>
+                                <td>Peso</td>
+                                <td>Par. jugados</td>
+                                <td>Goles</td>
+                                <td>Tar. amarillas</td>
+                                <td>Tar. rojas</td>
+                            </tr>
+                            @foreach($porteros as $player)
+                            <tr class="text-center">
+                                <td>
+                                    <a style="text-decoration: none; color: black;">
+                                        <img src="{{ $player['player']['photo'] }}" alt="" style="width: 35px; height: 35px;">
+                                    </a>
+                                </td>
+                                <th>
+                                    {{ $player['player']['name'] }}
+                                </th>
+                                <td>{{ $player['player']['age'] }}</td>
+                                <td>{{ $player['player']['height'] }}</td>
+                                <td>{{ $player['player']['weight'] }}</td>
+                                <td>{{ $player['statistics'][0]['games']['appearences'] }}</td>
+                                <th>{{ $player['statistics'][0]['goals']['total'] }}</th>
+                                <td>{{ $player['statistics'][0]['cards']['yellow'] }}</td>
+                                <td>{{ $player['statistics'][0]['cards']['red'] }}</td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <th colspan="9" class="fs-3">Defensas</th>
+                            </tr>
+                            <tr class="text-center" style="background-color: #eeeeee;">
+                                <td></td>
+                                <td>Nombre</td>
+                                <td>Edad</td>
+                                <td>Altura</td>
+                                <td>Peso</td>
+                                <td>Par. jugados</td>
+                                <td>Goles</td>
+                                <td>Tar. amarillas</td>
+                                <td>Tar. rojas</td>
+                            </tr>
+                            @foreach($defensas as $player)
+                            <tr class="text-center">
+                                <td>
+                                    <a style="text-decoration: none; color: black;">
+                                        <img src="{{ $player['player']['photo'] }}" alt="" style="width: 35px; height: 35px;">
+                                    </a>
+                                </td>
+                                <th>
+                                    {{ $player['player']['name'] }}
+                                </th>
+                                <td>{{ $player['player']['age'] }}</td>
+                                <td>{{ $player['player']['height'] }}</td>
+                                <td>{{ $player['player']['weight'] }}</td>
+                                <td>{{ $player['statistics'][0]['games']['appearences'] }}</td>
+                                <th>{{ $player['statistics'][0]['goals']['total'] }}</th>
+                                <td>{{ $player['statistics'][0]['cards']['yellow'] }}</td>
+                                <td>{{ $player['statistics'][0]['cards']['red'] }}</td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <th colspan="9" class="fs-3">Centrocampistas</th>
+                            </tr>
+                            <tr class="text-center" style="background-color: #eeeeee;">
+                                <td></td>
+                                <td>Nombre</td>
+                                <td>Edad</td>
+                                <td>Altura</td>
+                                <td>Peso</td>
+                                <td>Par. jugados</td>
+                                <td>Goles</td>
+                                <td>Tar. amarillas</td>
+                                <td>Tar. rojas</td>
+                            </tr>
+                            @foreach($centrocampistas as $player)
+                            <tr class="text-center">
+                                <td>
+                                    <a style="text-decoration: none; color: black;">
+                                        <img src="{{ $player['player']['photo'] }}" alt="" style="width: 35px; height: 35px;">
+                                    </a>
+                                </td>
+                                <th>
+                                    {{ $player['player']['name'] }}
+                                </th>
+                                <td>{{ $player['player']['age'] }}</td>
+                                <td>{{ $player['player']['height'] }}</td>
+                                <td>{{ $player['player']['weight'] }}</td>
+                                <td>{{ $player['statistics'][0]['games']['appearences'] }}</td>
+                                <th>{{ $player['statistics'][0]['goals']['total'] }}</th>
+                                <td>{{ $player['statistics'][0]['cards']['yellow'] }}</td>
+                                <td>{{ $player['statistics'][0]['cards']['red'] }}</td>
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <th colspan="9" class="fs-3">Delanteros</th>
+                            </tr>
+                            <tr class="text-center" style="background-color: #eeeeee;">
+                                <td></td>
+                                <td>Nombre</td>
+                                <td>Edad</td>
+                                <td>Altura</td>
+                                <td>Peso</td>
+                                <td>Par. jugados</td>
+                                <td>Goles</td>
+                                <td>Tar. amarillas</td>
+                                <td>Tar. rojas</td>
+                            </tr>
+                            @foreach($delanteros as $player)
+                            <tr class="text-center">
+                                <td>
+                                    <a style="text-decoration: none; color: black;">
+                                        <img src="{{ $player['player']['photo'] }}" alt="" style="width: 35px; height: 35px;">
+                                    </a>
+                                </td>
+                                <th>
+                                    {{ $player['player']['name'] }}
+                                </th>
+                                <td>{{ $player['player']['age'] }}</td>
+                                <td>{{ $player['player']['height'] }}</td>
+                                <td>{{ $player['player']['weight'] }}</td>
+                                <td>{{ $player['statistics'][0]['games']['appearences'] }}</td>
+                                <th>{{ $player['statistics'][0]['goals']['total'] }}</th>
+                                <td>{{ $player['statistics'][0]['cards']['yellow'] }}</td>
+                                <td>{{ $player['statistics'][0]['cards']['red'] }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('js')
+<script src="{{ asset('js/scriptsEquipos.js') }}"></script>
+@endsection
