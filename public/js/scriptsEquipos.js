694a5ef34fded7aa1ae9359d40a811a6a3b39bc2
devadrian16 <adri__16@hotmail.com>
2022-05-04 14:30:38 +0200

    equipos fetch

--- public/js/scriptsEquipos.js
+++ public/js/scriptsEquipos.js
@@ -6,12 +6,43 @@ document.addEventListener("DOMContentLoaded", function () {
     nofavorito.addEventListener("click", function (e) {
         favorito.classList.remove("d-none");
         e.currentTarget.classList.add("d-none");
+        agregar();
     });
 
     favorito.addEventListener("click", function (e) {
         nofavorito.classList.remove("d-none");
         e.currentTarget.classList.add("d-none");
+        eliminar();
     });
+
+    async function agregar() {
+        const res = await fetch('http://127.0.0.1:8000/save/529', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            //body: JSON.stringify(obj)
+        });
+
+        const data = await res.json();
+        console.log(data);
+    }
+
+
+    async function eliminar() {
+        const res = await fetch('http://127.0.0.1:8000/delete/529', {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            //body: JSON.stringify(obj)
+        });
+
+        const data = await res.json();
+        console.log(data);
+    }
 
 });
 
