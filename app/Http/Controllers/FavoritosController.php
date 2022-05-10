f0410e0a4244b74fee818197b82890081abc57ed
devadrian16 <adri__16@hotmail.com>
2022-05-10 14:06:30 +0200

    favoritos

--- app/Http/Controllers/FavoritosController.php
+++ app/Http/Controllers/FavoritosController.php
@@ -12,26 +12,28 @@ class FavoritosController extends Controller
 {
     private $api;
 
-	public function __construct(Client $client) 
-  {	
-		$this->api = new ApiController($client);
-	}
-
-  public function verFavoritos() {
-    //Equipos
-    $last = [];
-    $next = [];
-    $favoritos = Favorito::where('id_user', Auth::user()->id)->get();
-    foreach($favoritos as $favorito) {
-      $lastMatches = $this->api->getLastMatchesTeam($favorito->id_team, 2);
-      $nextMatches = $this->api->getNextMatchesTeam($favorito->id_team, 4);
-      array_push($last, $lastMatches);
-      array_push($next, $nextMatches);
+    public function __construct(Client $client)
+    {
+      $this->api = new ApiController($client);
     }
 
-    //Status
-    $status = $this->api->getStatus();
+    public function verFavoritos()
+    {
+      //Equipos
+      $last = [];
+      $next = [];
+      $favoritos = Favorito::where('id_user', Auth::user()->id)->get();
+      foreach ($favoritos as $favorito) {
+        $lastMatches = $this->api->getLastMatchesTeam($favorito->id_team, 2);
+        $nextMatches = $this->api->getNextMatchesTeam($favorito->id_team, 4);
+        array_push($last, $lastMatches);
+        array_push($next, $nextMatches);
+      }
 
-	  return view('favoritos', ['status' => $status, 'favoritos' => $favoritos, 'anteriores' => $last, 'siguientes' => $next]);
-  }
+      //Status
+      $status = $this->api->getStatus();
+
+      return view('favoritos', [
+        'status' => $status, 'favoritos' => $favoritos, 'anteriores' => $last, 'siguientes' => $next]);
+    }
 }
