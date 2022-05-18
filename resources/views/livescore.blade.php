154fb15b9395f131ebae97b2d834d1f56b1fc779
devadrian16 <adri__16@hotmail.com>
2022-05-18 10:58:44 +0200

    modal partidos

--- resources/views/livescore.blade.php
+++ resources/views/livescore.blade.php
@@ -61,7 +61,7 @@
                                         </td>
                                         <td class="col-1 text-end">
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+100 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -85,7 +85,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+100 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -121,7 +121,7 @@
                                         <td class="col-1 text-end">
                                         @if($match['fixture']['status']['short'] != 'NS')
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+100 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -145,7 +145,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+100 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -199,7 +199,7 @@
                                         </td>
                                         <td class="col-1 text-end">
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+200 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -223,7 +223,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+200 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -259,7 +259,7 @@
                                         <td class="col-1 text-end">
                                         @if($match['fixture']['status']['short'] != 'NS')
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+200 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -283,7 +283,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+200 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -337,7 +337,7 @@
                                         </td>
                                         <td class="col-1 text-end">
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+300 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -361,7 +361,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+300 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -397,7 +397,7 @@
                                         <td class="col-1 text-end">
                                         @if($match['fixture']['status']['short'] != 'NS')
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+300 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -421,7 +421,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+300 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -475,7 +475,7 @@
                                         </td>
                                         <td class="col-1 text-end">
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+400 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -499,7 +499,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+400 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
@@ -535,7 +535,7 @@
                                         <td class="col-1 text-end">
                                         @if($match['fixture']['status']['short'] != 'NS')
                                             <!-- Modal -->
-                                            <div class="modal fade" id="estadisticas{{ $loop->iteration+400 }}" tabindex="-1" role="dialog" aria-hidden="true">
+                                            <div class="modal fade" id="estadisticas{{ $match['fixture']['id'] }}" tabindex="-1" role="dialog" aria-hidden="true">
                                                 <div class="modal-dialog" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -559,7 +559,7 @@
                                                 </div>
                                             </div>
                                             <!-- Button modal -->
-                                            <div style="cursor: pointer;" onclick="clickModal('{{ $loop->iteration+400 }}')">
+                                            <div style="cursor: pointer;" onclick="clickModal('{{ $match['fixture']['id'] }}')">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
